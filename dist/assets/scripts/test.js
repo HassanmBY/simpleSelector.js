dd77c6a1fd3eb4d0fa9527687d8cd7911fdabaae
Mirza Hassan <36904595+HassanmBY@users.noreply.github.com>
2022-05-15 03:11:04 +0200

    Added animate method
    Added a way of animating selected elements via the .animate() method

---
+++ dist/assets/scripts/test.js
@@ -0,0 +1,18 @@
+const person = {
+    name: "John",
+    age: 30,
+    job: "teacher",
+    presentation: function(style, timeOfDay) {
+        if (style === "formal") {
+            console.log(
+                `Good ${timeOfDay}, Ladies and gentlemen! I'm ${this.name}, I'm a ${this.job} and I'm ${this.age} years old.`
+            );
+        } else if (style === "friendly") {
+            console.log(
+                `Hey! What's up? I'm ${this.name}, I'm a ${this.job} and I'm ${this.age} years old. Have a nice ${timeOfDay}.`
+            );
+        }
+    },
+};
+
+person.presentation("formal", "morning");
--- src/assets/scripts/libs/simpleSelector.js
+++ src/assets/scripts/libs/simpleSelector.js
@@ -1,40 +1,41 @@
-const attach = (nodeList) => {
+const attach = nodeList => {
     // DOCUMENT
     nodeList.ready = callback => {
         document.readyState != "loading" ?
             callback() :
             document.addEventListener("DOMContentLoaded", callback);
-    }
+    };
 
     // CONSOLE
     nodeList.log = () => {
         console.log(nodeList[0]);
-    }
+    };
     nodeList.logList = () => {
         for (node of nodeList) console.log(node);
-
-    }
+    };
     nodeList.logNode = () => {
         console.log(nodeList);
-
-    }
+    };
     nodeList.logCompStyle = () => {
         for (node of nodeList) console.log(getComputedStyle(node));
-    }
+    };
 
     // HTML
-    nodeList.text = input => {
+    nodeList.textContent = input => {
         for (node of nodeList) node.textContent = input;
-    }
+    };
+    nodeList.text = input => {
+        for (node of nodeList) node.innerText = input;
+    };
     nodeList.html = input => {
         for (node of nodeList) node.innerHTML = input;
-    }
+    };
     nodeList.addHtml = input => {
         for (node of nodeList) node.innerHTML += input;
-    }
+    };
     nodeList.append = input => {
         for (node of nodeList) node.appendChild(input);
-    }
+    };
 
     // CSS
     nodeList.css = (...args) => {
@@ -42,68 +43,100 @@ const attach = (nodeList) => {
             for (node of nodeList)
                 for (const o of args)
                     for (const args in o) node.style[args] = o[args];
-        else if (typeof args[0] == "string" && typeof args[1] == "string" && args.length == 2)
+        else if (
+            typeof args[0] == "string" &&
+            typeof args[1] == "string" &&
+            args.length == 2
+        )
             for (node of nodeList) node.style[args[0]] = args[1];
         else if ("string" == typeof args[0] && 1 == args.length)
-            for (node of nodeList) return `${args}: ${window.getComputedStyle(node)[args]||"Not defined"}`
-    }
+            for (node of nodeList)
+                return `${args}: ${window.getComputedStyle(node)[args] || "Not defined"}`;
+    };
     nodeList.hide = () => {
         for (node of nodeList) {
-            node.style.display = 'none';
-            node.style.visibility = 'hidden';
+            node.style.display = "none";
+            node.style.visibility = "hidden";
         }
-    }
+    };
     nodeList.show = () => {
         for (node of nodeList) {
-            node.tagName == 'LI' | 'li' ?
-                node.style.display = 'list-item' :
-                node.style.display = 'block';
-            node.style.visibility = 'visible';
+            (node.tagName == "LI") | "li" ?
+            (node.style.display = "list-item") :
+            (node.style.display = "block");
+            node.style.visibility = "visible";
         }
-    }
+    };
     nodeList.hideShow = () => {
         for (node of nodeList)
             if (getComputedStyle(node).display == "none") {
-                node.tagName == 'LI' | 'li' ?
-                    node.style.display = 'list-item' :
-                    node.style.display = 'block';
-                node.style.visibility = 'visible';
+                (node.tagName == "LI") | "li" ?
+                (node.style.display = "list-item") :
+                (node.style.display = "block");
+                node.style.visibility = "visible";
             } else {
-                node.style.display = 'none';
-                node.style.visibility = 'hidden';
+                node.style.display = "none";
+                node.style.visibility = "hidden";
             }
-    }
+    };
     nodeList.addClass = className => {
         for (node of nodeList) node.classList.add(className);
-    }
+    };
     nodeList.removeClass = className => {
         for (node of nodeList) node.classList.remove(className);
-    }
+    };
     nodeList.toggleClass = className => {
         for (node of nodeList) node.classList.toggle(className);
-    }
+    };
     nodeList.replaceClass = (oldClassName, newClassName) => {
         for (node of nodeList) node.classList.replace(oldClassName, newClassName);
-    }
+    };
     nodeList.classList = () => {
         for (node of nodeList) return node.classList;
-    }
+    };
     nodeList.hasClass = className => {
         for (node of nodeList) return node.classList.contains(className);
-    }
+    };
 
     // EVENT LISTENERS
     nodeList.click = fct => {
-        for (node of nodeList) node.addEventListener('click', fct);
-    }
+        for (node of nodeList) node.addEventListener("click", fct);
+    };
 
     nodeList.on = (listener, fct) => {
-        for (node of nodeList) node.addEventListener(listener, fct)
-    }
+        for (node of nodeList) node.addEventListener(listener, fct);
+    };
 
     nodeList.hover = (over, out) => {
-        for (node of nodeList) node.addEventListener('mouseover', over), node.addEventListener('mouseout', out);
-    }
+        for (node of nodeList)
+            node.addEventListener("mouseover", over),
+            node.addEventListener("mouseout", out);
+    };
+
+    // ANIMATE
+
+    nodeList.animate = (
+        [...args],
+        duration = 1500,
+        easing = "linear",
+        delay = 0,
+        iteration = 1,
+        direction = "normal",
+        fill = "forwards"
+    ) => {
+        duration *= 1000;
+        delay *= 1000;
+        for (node of nodeList) {
+            node.animate([...args], {
+                duration,
+                easing,
+                delay,
+                iteration,
+                direction,
+                fill,
+            });
+        }
+    };
 
     // Custom fct on any selector
     nodeList.each = callback => {
@@ -112,47 +145,41 @@ const attach = (nodeList) => {
             const boundFn = callback.bind(elem);
             boundFn(i, elem);
         }
-    }
-}
+    };
+};
 const µ = (...args) => {
-    if (typeof args[0] === 'function') {
+    if (typeof args[0] === "function") {
         const readyFn = args[0];
-        document.addEventListener('DOMContentLoaded', readyFn);
-    } else if (typeof args[0] === 'string') {
-        let selector = args[0]
+        document.addEventListener("DOMContentLoaded", readyFn);
+    } else if (typeof args[0] === "string") {
+        let selector = args[0];
         let nodeList = document.querySelectorAll(selector);
         attach(nodeList);
 
-        return nodeList
-
+        return nodeList;
     } else if (args[0] instanceof HTMLElement) {
         let element = [args[0]];
-        attach(element)
+        attach(element);
         return element;
-
-    } else if (typeof args[0] === 'undefined') {
+    } else if (typeof args[0] === "undefined") {
         // BUFFER ??
-        let doc = document
+        let doc = document;
         doc.create = (elemTag, className, content, idName) => {
             let elem = document.createElement(elemTag);
-            className && (elem.classList.add(className))
-            idName && (elem.setAttribute('id', idName));
+            className && elem.classList.add(className);
+            idName && elem.setAttribute("id", idName);
             content && (elem.textContent = content);
             return elem;
-        }
+        };
 
         // AJAX
 
         doc.ajax = (url, options, responseFct, dataFct, catchFct) => {
-            return fetch(url, options)
-                .then(responseFct)
-                .then(dataFct)
-                .catch(catchFct)
-                /*
+            return fetch(url, options).then(responseFct).then(dataFct).catch(catchFct);
+            /*
                 Base Catch Example: err => { console.error('Error:', err)}
                 */
-        }
+        };
         return doc;
     }
-
-}
+};
